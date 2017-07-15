8c6129dc77d90d46fd42d022c2aebe37a732ea3d
hero <xiaofeng.zheng@calmcar.com>
2017-07-15 16:22:15 +0800

    change target program print format

--- distance_analysis.py
+++ distance_analysis.py
@@ -13,9 +13,9 @@ import sys
 class DisAna:
     '''printf("pre obj dis.y:%f, cur obj dis.y:%f, pre pre obj dis.y:%f ID:%d\n", tracking_buffer_[tracking_buffer_.size() - 1].distance_.y, cur, tracking_buffer_[tracking_buffer_.size() - 2].distance_.y, id_); '''
     def __init__(self, filename):
-        self.preobjdis = {"":[]}
-        self.curobjdis = {"":[]}
-        self.prepreobjdis = {"":[]}
+        self.preobjdis = {}
+        self.curobjdis = {}
+        self.prepreobjdis = {}
         self.prelist = []
         self.curlist = []
         self.preprelist = []
@@ -26,37 +26,37 @@ class DisAna:
     def GetPreObjDistance(self):
         f = open(self.filename, 'r')
         contant = f.readlines()
+        print(len(contant))
         preid=0
         for line in contant :
             m = line.split(' ')
-            ids = m[10].split(':')
-            if (preid != int(ids[1][:-1])):
-                preid = int(ids[1][:-1])
-                self.preobjdis[str(preid)] = []
-                self.curobjdis[str(preid)] = []
-                self.prepreobjdis[str(preid)] = []
-            prestr = m[2].split(':')
-            curstr = m[5].split(':')
-            prepre = m[9].split(':')
-            self.preobjdis[str(preid)].append(prestr[1][:-1])
-            self.curobjdis[str(preid)].append(curstr[1][:-1])
-            self.prepreobjdis[str(preid)].append(prepre[1][:-1])
-        print("prepre:%s"%len(self.prepreobjdis["1"]))
-        index = [x for x in self.preobjdis]
-        index.sort()
+            ids = m[3]
+            if (ids not in self.preobjdis.keys()):
+                print(ids in self.preobjdis.keys())
+                #print(preid, self.preobjdis.keys())
+                self.preobjdis[ids] = []
+                self.curobjdis[ids] = []
+                self.prepreobjdis[ids] = []
+            pre = m [0]
+            cur = m [1]
+            prepre = m [2]
+            self.preobjdis[ids].append(pre)
+            self.curobjdis[ids].append(cur)
+            self.prepreobjdis[ids].append(prepre)
+        index = [int(x) for x in self.preobjdis]
+        index = sorted(index)
         for x in index:
             self.prelist.extend(self.preobjdis[str(x)])
             self.curlist.extend(self.curobjdis[str(x)])
             self.preprelist.extend(self.prepreobjdis[str(x)])
-            print(len(self.prepreobjdis[str(x)]))
+            #print (x, len(self.prepreobjdis[str(x)]), self.prepreobjdis[str(x)])
         print(len(self.prelist))
         print(len(self.curlist))
         print(len(self.preprelist))
 
     def ConfPlt(self):
         x = [x for x in range(len(self.prelist))]
-        diff = [float(self.curlist[x]) - float(self.preprelist[x]) for x in range(len(self.prelist))]
-        print(len(diff))
+        diff = [float(self.curlist[i]) - float(self.preprelist[i]) for i in range(len(self.prelist))]
         plt.figure(figsize = (8, 4))
         plt.plot(x, self.prelist,":", label="$PreObjectDis$", color="green", linewidth=1)
         plt.plot(x, self.curlist,".", label="$CurObjectDis$", color="blue", linewidth=1)
